6e06a1e989d1ee31ba396b00001891b41cef186a
Connor Pelkey <pelkey.c@husky.neu.edu>
2018-03-27 03:11:13 +0000

    wip

--- room_stats.py
+++ room_stats.py
@@ -2,34 +2,31 @@
 
 import sys
 import Adafruit_DHT
-
-
-class RoomStats:
-
-    def __init__(self, interval=1):
-        self.interval = interval
-        self.data = []
-    
-    def fetch_DHT(self):
-        humidity, temp = Adafruit_DHT.read_retry(11, 4)
-        temp = temp * 1.8 + 32
-        return temp, humidity
-
-    def collect(self):
-        while True: 
-            t, h = fetch_DHT()
-            print_reading(t, h)
-            self.data.append({temperature: t, humidity: h}) 
-
-    def print_reading(temperature, humidity):
-        print '{0:.1f} F, {1:.1f} %'.format(temperature, humidity)
-
+from RPLCD.gpio import CharLCD
+import RPi.GPIO as GPIO
 
 def main():
-    cred = credentials.Certificate('./.credentials.json')
-    default_app = firebase_admin.initialize_app(cred)
+    GPIO.setwarnings(False)
+    lcd = init_lcd() 
 
-    RoomStats().collect()
+    while True:
+        humid, temp = Adafruit_DHT.read_retry(11, 4)
+        temp = to_fahrenheit(temp)
+        write_lcd(lcd, temp, humid)
+
+def init_lcd():
+    ''' Create the LCD with BCM pin numberings 
+        NOTE: correct pin values must be used '''
+    return CharLCD(pin_rs=22, pin_rw=24, pin_e=23, pins_data=[9, 25, 11, 8], numbering_mode=GPIO.BCM)
+
+def write_lcd(lcd, temp, humid):
+    lcd.clear()
+    lcd.write_string("Temp:     {0:.1f}F".format(temp))
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("Humidity: {0:.1f}%".format(humid))
+
+def to_fahrenheit(temp):
+    return temp*1.8 + 32
 
 if __name__ == '__main__':
     main()
